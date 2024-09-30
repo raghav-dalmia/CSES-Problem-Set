134cfe1210ad65a1b644a6ecd4973e6e2220fcd5
Raghav Dalmia <raghavdalmia@Raghavs-Mac-mini.local>
2024-09-30 23:03:39 +0530

    solution of Weird Algorithm

---
+++ IntroductoryProblems/WeirdAlgorithm.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin>>n;
+    cout<<n<<" ";
+    while(n!=1) {
+        n = (n&1) ? (n*3)+1 : n/2;
+        cout<<n<<" ";
+    }
+}
+/*`
+
+</RD>
+
+*/
